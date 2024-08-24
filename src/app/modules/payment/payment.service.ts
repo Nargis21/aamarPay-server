f146e40abfd225b42b0bcccc4e24e9894e541098
nargis21 <nargisakther1933@gmail.com>
2024-08-24 21:12:08 +0600

    customize payment confirmation template added

--- src/app/modules/payment/payment.service.ts
+++ src/app/modules/payment/payment.service.ts
@@ -1,17 +1,28 @@
+import { join } from "path";
 import orderModel from "../order/order.model";
 import { verifyPayment } from "./payment.utils";
+import { readFileSync } from "fs";
 
 const confirmationService = async (transactionId: string, status: string) => {
   const verifyResponse = await verifyPayment(transactionId);
   let result;
+  let message;
+
   if (verifyResponse && verifyResponse.pay_status === "Successful") {
     result = await orderModel.findOneAndUpdate(
       { transactionId },
       { paymentStatus: "Paid" }
     );
+    message = "Payment Successful!😎";
+  } else {
+    message = "Payment Failed!😪";
   }
 
-  return `<h1>Payment ${status}</h1>`;
+  const filePath = join(__dirname, "../../../views/confirmation.html");
+  let template = readFileSync(filePath, "utf-8");
+  template = template.replace("{{message}}", message);
+
+  return template;
 };
 
 export const paymentService = { confirmationService };
