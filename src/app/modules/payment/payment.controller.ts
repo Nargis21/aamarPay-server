cbcf06dee606f68e722e8e96447fcd5fd956b482
nargis21 <nargisakther1933@gmail.com>
2024-08-24 19:41:12 +0600

    configuring fail and cancel url's

--- src/app/modules/payment/payment.controller.ts
+++ src/app/modules/payment/payment.controller.ts
@@ -1,11 +1,14 @@
 import { Request, Response } from "express";
 import { paymentService } from "./payment.service";
 
-const confirmationController = (req: Request, res: Response) => {
-  const { transactionId } = req.query;
+const confirmationController = async (req: Request, res: Response) => {
+  const { transactionId, status } = req.query;
 
-  res.send(`<h1>Payment Success</h1>`);
-  const result = paymentService.confirmationService(transactionId as string);
+  const result = await paymentService.confirmationService(
+    transactionId as string,
+    status as string
+  );
+  res.send(result);
 };
 
 export const paymentController = {
--- src/app/modules/payment/payment.service.ts
+++ src/app/modules/payment/payment.service.ts
@@ -1,17 +1,17 @@
 import orderModel from "../order/order.model";
 import { verifyPayment } from "./payment.utils";
 
-const confirmationService = async (transactionId: string) => {
+const confirmationService = async (transactionId: string, status: string) => {
   const verifyResponse = await verifyPayment(transactionId);
   let result;
-  if (verifyResponse.pay_status === "Successful") {
+  if (verifyResponse && verifyResponse.pay_status === "Successful") {
     result = await orderModel.findOneAndUpdate(
       { transactionId },
       { paymentStatus: "Paid" }
     );
   }
 
-  return result;
+  return `<h1>Payment ${status}</h1>`;
 };
 
 export const paymentService = { confirmationService };
