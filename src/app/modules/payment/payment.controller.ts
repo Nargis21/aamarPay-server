a70717cac18c615c23d8c4e9cd1f0d369a1ec32f
nargis21 <nargisakther1933@gmail.com>
2024-08-24 12:25:55 +0600

    update payment status in db

--- src/app/modules/payment/payment.controller.ts
+++ src/app/modules/payment/payment.controller.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from "express";
+import { paymentService } from "./payment.service";
 
 const confirmationController = (req: Request, res: Response) => {
+  const { transactionId } = req.query;
+
   res.send(`<h1>Payment Success</h1>`);
+  const result = paymentService.confirmationService(transactionId as string);
 };
 
 export const paymentController = {
---
+++ src/app/modules/payment/payment.service.ts
@@ -0,0 +1,12 @@
+import orderModel from "../order/order.model";
+
+const confirmationService = async (transactionId: string) => {
+  const result = await orderModel.findOneAndUpdate(
+    { transactionId },
+    { paymentStatus: "Paid" }
+  );
+
+  return result;
+};
+
+export const paymentService = { confirmationService };
